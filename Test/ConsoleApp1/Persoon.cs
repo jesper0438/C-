dd06ffa4fed58f5f0768f6730e1373f081e8bfc4
jesper0438 <robr0002@hz.nl>
2017-11-13 16:21:30 +0100

    Renamed class and added more information

---
+++ Test/ConsoleApp1/Persoon.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Persoon
+    {
+        private string Naam;
+        private int Leeftijd;
+        private string Woonplaats;
+        private string Huisdier;
+
+        //Constructor
+        public Persoon(string naam, int leeftijd, string woonplaats, string huisdier)
+        {
+            this.Naam = naam;
+            this.Leeftijd = leeftijd;
+            this.Woonplaats = woonplaats;
+            this.Huisdier = huisdier;
+        }
+
+        public string GeefPersoonsNaam()
+        {
+            return this.Naam;
+        }
+
+        public int GeefLeefijd()
+        {
+            return this.Leeftijd;
+        }
+
+        public string GeefWoonplaats()
+        {
+            return this.Woonplaats;
+        }
+
+        public string GeefHuisdier()
+        {
+            return this.Huisdier;
+        }
+
+
+    }
+}
--- Test/ConsoleApp1/Program.cs
+++ Test/ConsoleApp1/Program.cs
@@ -6,17 +6,35 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Student mijnStudent = new Student(12345,"Gesper", "SRE1a");
-            Student deStudent = new Student(123455,"erwin", "alles");
+            Student mijnStudent = new Student(117476,"Jesper", "IAN1");
+            Student deStudent = new Student(123455,"Erwin", "SRE1a");
+            Persoon ikPersoon = new Persoon("Jeppie", 21, "Napoli", "Woef");
 
-            // Dit gaat over mijn huis
+            // Dit gaat over mij als student
+            Console.WriteLine("Studentnummer");
             Console.WriteLine(mijnStudent.GeefStudentNummer());
+            Console.WriteLine("Studentnaam");
             Console.WriteLine(mijnStudent.GeefStudentnaam());
+            Console.WriteLine("Competentie");
             Console.WriteLine(mijnStudent.GeefCompetenties());
-
+            // Dit gaat over erwin als student
+            Console.WriteLine("Studentnummer");
             Console.WriteLine(deStudent.GeefStudentNummer());
+            Console.WriteLine("Studentnaam");
             Console.WriteLine(deStudent.GeefStudentnaam());
+            Console.WriteLine("Competentie");
             Console.WriteLine(deStudent.GeefCompetenties());
+            // Dit gaat over mij als persoon
+            Console.WriteLine("Naam");
+            Console.WriteLine(ikPersoon.GeefPersoonsNaam());
+            Console.WriteLine("Leeftijd");
+            Console.WriteLine(ikPersoon.GeefLeefijd());
+            Console.WriteLine("Woonplaats");
+            Console.WriteLine(ikPersoon.GeefWoonplaats());
+            Console.WriteLine("Huisdier");
+            Console.WriteLine(ikPersoon.GeefHuisdier());
+
+
 
             string name = Console.ReadLine();
             Console.Read();
